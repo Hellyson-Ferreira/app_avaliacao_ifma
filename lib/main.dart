547cd101e0273bfa6a5de73209a1e7caf5ea001a
Hellyson Ferreira <hellysonferreira.hf1@gmail.com>
2019-12-19 16:39:15 -0300

    Aquivos upados

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:app_avaliacao_ifma/LoginAluno/boas_vindas.dart';
+import 'package:app_avaliacao_ifma/LoginAluno/login_page_aluno.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  final routes = <String, WidgetBuilder>{
+    LoginPageAluno.tag: (context) => LoginPageAluno(),
+    BoasVindasAluno.tag: (context) => BoasVindasAluno(),
+
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return MaterialApp( 
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.lightGreen,
+        fontFamily: 'Nunito',
+      ),
+      home:LoginPageAluno(),
+      routes: routes,
+    );
+  }
+}
