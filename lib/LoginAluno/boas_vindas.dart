5ba8744374ed8d281971a2a27ee3cd6d5e6f3a98
Hellyson Ferreira <hellysonferreira.hf1@gmail.com>
2019-12-20 22:41:05 -0300

    Ajuste na tela de boas vindas

--- lib/LoginAluno/boas_vindas.dart
+++ lib/LoginAluno/boas_vindas.dart
@@ -37,6 +37,7 @@ Although never is often better than *right* now.
 If the implementation is hard to explain, it's a bad idea.
 If the implementation is easy to explain, it may be a good idea.
 Namespaces are one honking great idea -- let's do more of those!
+Python
 ''',
         style: TextStyle(fontSize: 16.0, color: Colors.white),
       ),
